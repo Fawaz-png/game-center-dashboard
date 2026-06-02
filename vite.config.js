103be6d60f7cb5dd2da4663930c13cea8bf57e13
Fawaz <67784280+Fawaz-png@users.noreply.github.com>
2026-06-02 15:53:30 +0000

    chore(env): downgrade node and expose vite ports for codespaces

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,21 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+
+    // 1. Add this entire server block
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        cors: true,
+        origin: 'https://bug-free-space-guide-j97j44v569j2qjqg-5173.app.github.dev',
+        hmr: {
+            host: 'bug-free-space-guide-j97j44v569j2qjqg-5173.app.github.dev',
+            clientPort: 443,
+            protocol: 'wss'
+        }
+    },
+
     plugins: [
         laravel({
             input: 'resources/js/app.js',
